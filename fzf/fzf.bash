ce303a38a996e340abf0212d935ed52b7ba44c16
bbourgoine <brent.bourgoine@gmail.com>
2020-06-08 22:24:00 -0400

    Adding conditionals to fzf as some systems won't have it

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -6,8 +6,12 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+if [ -f /usr/local/opt/fzf/shell/completion.bash ]; then
+   [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+fi
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+if [ -f /usr/local/opt/fzf/shell/key-bindings.bash ]; then
+   source "/usr/local/opt/fzf/shell/key-bindings.bash"
+fi
